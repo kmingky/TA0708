53dc7925cf10c6c10112e61b2bf79dc20512f16c
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-07-08 16:35:09 +0900

    Add simpleJWT

--- user/views.py
+++ user/views.py
@@ -32,23 +32,23 @@ class SignUpView(APIView):
         return Response(status=status.HTTP_200_OK)
 
 
-class SignInView(APIView):
-    # 로그인
-    def post(self, request):
-        username = request.data.get('email', '')
-        password = request.data.get('password', '')
+# class SignInView(APIView):
+#     # 로그인
+#     def post(self, request):
+#         username = request.data.get('email', '')
+#         password = request.data.get('password', '')
 
-        user = authenticate(request, email=username, password=password)
-        if not user:
-            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+#         user = authenticate(request, email=username, password=password)
+#         if not user:
+#             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        login(request, user)
-        now = datetime.datetime.now()
-        UserLog.objects.create(user=user, last_login_date=now.strftime('%Y-%m-%d')) # 유저 로그 생성
-        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
+#         login(request, user)
+#         now = datetime.datetime.now()
+#         UserLog.objects.create(user=user, last_login_date=now.strftime('%Y-%m-%d')) # 유저 로그 생성
+#         return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
 
 
-    def delete(self, request):
-        logout(request)
-        return Response({"message": "logout success!!"}, status=status.HTTP_200_OK)
+#     def delete(self, request):
+#         logout(request)
+#         return Response({"message": "logout success!!"}, status=status.HTTP_200_OK)
 
